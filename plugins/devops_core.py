8e1f47d8220fd44ac87b95ea50858556b679041c
Mayuri <Mayuri.Balasubramaniam@maersk.com>
2018-03-01 17:15:08 +0530

    serialized variables

--- plugins/devops_core.py
+++ plugins/devops_core.py
@@ -34,7 +34,6 @@ class DevopsPlugin(Plugin):
     token_class = Token
 
     def ingest(self, key, value):
-       
         value['ts'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%s')
         mapper_tasks.message_producer(key,value)
 
